ee89b67f488b31bccb4d0db2a229587ee3ae05f2
tyagib <bhawna.mscs@gmail.com>
2016-03-03 11:42:34 -0800

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,4 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var awesomeThoughts = "I am Bhawna and I am AWESOME!";
+var funThoughts = "I am Bhawna and I am FUN!";
+console.log(awesomeThoughts);
+console.log(funThoughts);
